8e3de1df6006b5c2daaec61ebf266cf1fe80d8bd
Roman Rudenko <ruroma18@gmail.com>
2022-02-13 20:27:54 +0200

    Change function accelerate and brake

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,23 @@
 'use strict'
 
-function Car(manufacture, model, color, releaseYear, speed, maxSpeed, fuelConsumption, volume) {
+function Car(manufacture, model, color, releaseYear, maxSpeed, fuelConsumption, volume) {
   if (typeof manufacture !== 'string' ||
     typeof model !== 'string' ||
     typeof color !== 'string') {
     throw new TypeError('Enter strings')
   } else if (typeof releaseYear !== 'number' ||
-    typeof speed !== 'number' ||
     typeof maxSpeed !== 'number' ||
     typeof fuelConsumption !== 'number' ||
     typeof volume !== 'number') {
     throw new TypeError('Enter number')
-  } else if (speed < 0 || maxSpeed < 0) {
+  } else if (maxSpeed < 0) {
     throw new Error('Speed mustn`t be a negative')
   }
   this.manufacture = manufacture;
   this.model = model;
   this.color = color;
   this.releaseYear = releaseYear;
-  this.speed = speed;
+  this.speed = 0;
   this.maxSpeed = maxSpeed;
   this.fuelConsumption = fuelConsumption;
   this.volume = volume;
@@ -27,18 +26,18 @@ function Car(manufacture, model, color, releaseYear, speed, maxSpeed, fuelConsum
 
 function CarPrototype() {
   this.accelerate = function (speedNum) {
-    if (this.speed < this.maxSpeed) {
+    if ((this.speed + speedNum) <= this.maxSpeed) {
       return this.speed += speedNum;
     } else {
-      return false;
+      return this.speed;
     }
   };
 
   this.brake = function (speedNum) {
-    if (this.speed > 0) {
+    if ((this.speed - speedNum) >= 0) {
       return this.speed -= speedNum;
     } else {
-      return false;
+      return this.speed;
     }
   };
 
@@ -49,6 +48,6 @@ function CarPrototype() {
 
 Car.prototype = new CarPrototype;
 
-const car1 = new Car('audi', 'a7', 'black', 2018, 10, 220, 10.2, 69);
-const car2 = new Car('bmw', '5', 'white', 2020, 0, 300, 9, 60);
+const car1 = new Car('audi', 'a7', 'black', 2018, 220, 10.2, 69);
+const car2 = new Car('bmw', '5', 'white', 2020, 300, 9, 60);
 
